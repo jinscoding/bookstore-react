36266fda431ce1eb957645e09cbab9f8a1ec9253
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-14 19:23:22 +0900

    Button 컴포넌트 생성 및 테스트

--- book-store-fe/src/pages/Home.tsx
+++ book-store-fe/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/common/Button";
 import Title from "../components/common/Title";
 
 function Home() {
@@ -6,6 +7,9 @@ function Home() {
       <Title size='medium' color='background'>
         제목 테스트
       </Title>
+      <Button size='large' scheme='normal'>
+        버튼 테스트
+      </Button>
       <div> home body</div>
     </>
   );
