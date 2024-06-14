0d25988d6f84221a9ac8758a42b317713bc0a556
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-14 16:42:31 +0900

    테마 컨텍스트 설정

--- book-store-fe/src/App.tsx
+++ book-store-fe/src/App.tsx
@@ -1,21 +1,16 @@
-import { ThemeProvider } from "styled-components";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
-import { GlobalStyle } from "./style/global";
-import { ThemeName, getTheme, light } from "./style/theme";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
-import { useState } from "react";
+import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
-  const [themeName, setThemeName] = useState<ThemeName>("light");
   return (
-    <ThemeProvider theme={getTheme(themeName)}>
-      <GlobalStyle themeName={themeName} />
-      <ThemeSwitcher themeName={themeName} setThemeName={setThemeName} />
+    <BookStoreThemeProvider>
+      <ThemeSwitcher />
       <Layout>
         <Home />
       </Layout>
-    </ThemeProvider>
+    </BookStoreThemeProvider>
   );
 }
 
--- book-store-fe/src/components/header/ThemeSwitcher.tsx
+++ book-store-fe/src/components/header/ThemeSwitcher.tsx
@@ -1,14 +1,9 @@
-import { ThemeName } from "../../style/theme";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/themeContext";
 
-interface Props {
-  themeName: ThemeName;
-  setThemeName: (themeName: ThemeName) => void;
-}
+function ThemeSwitcher() {
+  const { themeName, toggleTheme } = useContext(ThemeContext);
 
-function ThemeSwitcher({ themeName, setThemeName }: Props) {
-  const toggleTheme = () => {
-    setThemeName(themeName === "light" ? "dark" : "light");
-  };
   return <button onClick={toggleTheme}>{themeName}</button>;
 }
 
