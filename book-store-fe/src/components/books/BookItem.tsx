8a044b9a343b7d0ce32d5cc43b6d96de19a2e4c7
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-20 22:43:05 +0900

    BookItem 작성

--- book-store-fe/src/components/books/BookItem.tsx
+++ book-store-fe/src/components/books/BookItem.tsx
@@ -1,10 +1,30 @@
 import styled from "styled-components";
+import { Book } from "../../models/book.model";
+import { formatNumber } from "../../utils/format";
+import { getImgSrc } from "../../utils/image";
+import { FaHeart } from "react-icons/fa";
 
-function BookItem() {
+interface Props {
+  book: Book;
+}
+
+function BookItem({ book }: Props) {
   return (
-    <div>
-      <h1>BookItem</h1>
-    </div>
+    <BookItemStyle>
+      <div className='lmg'>
+        <img src={getImgSrc(book.id)} alt={book.title} />
+      </div>
+      <div className='content'>
+        <h2 className='title'>{book.title}</h2>
+        <p className='summary'>{book.summary}</p>
+        <p className='author'>{book.author}</p>
+        <p className='price'>{formatNumber(book.price)}원</p>
+        <div className='likes'>
+          <FaHeart />
+          <span>{book.likes}</span>
+        </div>
+      </div>
+    </BookItemStyle>
   );
 }
 const BookItemStyle = styled.div``;
--- book-store-fe/src/components/books/BooksList.tsx
+++ book-store-fe/src/components/books/BooksList.tsx
@@ -1,10 +1,28 @@
 import styled from "styled-components";
+import BookItem from "./BookItem";
+import { Book } from "../../models/book.model";
 
+const dummyBook: Book = {
+  id: 1,
+  title: "Dummy Book",
+  img: 5,
+  category_id: 1,
+  summary: "Dummy Summary",
+  author: "Dummy Author",
+  price: 10000,
+  likes: 1,
+  form: "paperback",
+  isbn: "Dummy ISBN",
+  detail: "Dummy Detail",
+  pages: 100,
+  contents: "Dummy Contents",
+  pubDate: "2021-01-01",
+};
 function BooksList() {
   return (
-    <div>
-      <h1>BooksList</h1>
-    </div>
+    <BooksListStyle>
+      <BookItem book={dummyBook} />
+    </BooksListStyle>
   );
 }
 const BooksListStyle = styled.div``;
