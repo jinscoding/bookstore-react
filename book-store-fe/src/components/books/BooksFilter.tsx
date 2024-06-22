1421fec7e46cfbfdd2b5fd047f4ef0decb368eca
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-22 21:58:16 +0900

    refactor: querystring 상수로 관리
    querystring에 category_id와 news 따로 관리

--- book-store-fe/src/components/books/BooksFilter.tsx
+++ book-store-fe/src/components/books/BooksFilter.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Button from "../common/Button";
 import { useSearchParams } from "react-router-dom";
 import { useCategory } from "../../hooks/useCateogry";
+import { QUERYSTRING } from "../../constants/querystring";
 
 function BooksFilter() {
   const { category } = useCategory();
@@ -11,9 +12,9 @@ function BooksFilter() {
     const newSearchParams = new URLSearchParams(searchParams);
 
     if (id === null) {
-      newSearchParams.delete("category_id");
+      newSearchParams.delete(QUERYSTRING.CATEGORY_ID);
     } else {
-      newSearchParams.set("category_id", id.toString());
+      newSearchParams.set(QUERYSTRING.CATEGORY_ID, id.toString());
     }
 
     setSearchParams(newSearchParams);
@@ -22,10 +23,10 @@ function BooksFilter() {
   const handleNews = () => {
     const newSearchParams = new URLSearchParams(searchParams);
 
-    if (newSearchParams.get("news")) {
-      newSearchParams.delete("news");
+    if (newSearchParams.get(QUERYSTRING.NEWS)) {
+      newSearchParams.delete(QUERYSTRING.NEWS);
     } else {
-      newSearchParams.set("news", "true");
+      newSearchParams.set(QUERYSTRING.NEWS, "true");
     }
 
     setSearchParams(newSearchParams);
@@ -48,7 +49,7 @@ function BooksFilter() {
       <div className='new'>
         <Button
           size='medium'
-          scheme={searchParams.get("news") ? "primary" : "normal"}
+          scheme={searchParams.get(QUERYSTRING.NEWS) ? "primary" : "normal"}
           onClick={() => handleNews()}
         >
           신간
