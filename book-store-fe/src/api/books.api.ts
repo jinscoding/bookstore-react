84d4faa384b483354964cd3c89ba1302828e0641
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-23 19:24:10 +0900

    feat: 도서 정보 리스트 구현

---
+++ book-store-fe/src/api/books.api.ts
@@ -0,0 +1,32 @@
+import { Book } from "../models/book.model";
+import { httpClient } from "./http";
+import { Pagination } from "../models/pagination.model";
+
+interface FetchBooksParams {
+  category_id?: number;
+  news?: boolean;
+  currentPage?: number;
+  limit: number;
+}
+
+interface FetchBooksResponse {
+  books: Book[];
+  pagination: Pagination;
+}
+
+export const fetchBooks = async (params: FetchBooksParams) => {
+  try {
+    const response = await httpClient.get<FetchBooksResponse>("/books", {
+      params: params,
+    });
+    return response.data;
+  } catch (error) {
+    return {
+      books: [],
+      pagination: {
+        totalCount: 0,
+        currentPage: 1,
+      },
+    };
+  }
+};
